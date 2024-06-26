31e9037371455aafa60974dadb28b3bb5eb06d3f
dbiguenet <110406974+dbiguenet@users.noreply.github.com>
2024-06-26 09:07:07 +0200

    Add reference model in RModS

--- python/reality_apis/RModS/job_specifications.py
+++ python/reality_apis/RModS/job_specifications.py
@@ -100,6 +100,92 @@ class TouchupFormat(Enum):
     OBJ = 0
     DGN = 1
 
+class ReferenceModelType(Enum):
+    Orthophoto = 0
+    Complete = 1
+
+class TilingMode(Enum):
+    NoTiling = 0
+    RegularPlanarGrid = 1
+    RegularVolumetricGrid = 2
+    Adaptive = 3
+
+class GeometricPrecision(Enum):
+    Draft = 0
+    Medium = 1
+    High = 2
+    Extra = 3
+    Ultra = 4
+
+class PairSelection(Enum):
+    Generic = 0
+    StructuredAerial = 1
+    RegionOfInterest = 2
+
+class PhotoUsedForGeometry(Enum):
+    ExcludeThermal = 0
+    IncludeThermal = 1
+    NoPhoto = 2
+
+class HoleFilling(Enum):
+    SmallHoles = 0
+    AllHoles = 1
+
+class Simplification(Enum):
+    Standard = 0
+    PlanarRelative = 1
+    PlanarAbsolute = 2
+
+class ColorCorrection(Enum):
+    NoCorrection = 0
+    Standard = 1
+    StandardWithThermal = 2
+    BlockWise = 3
+    BlockWiseWithThermal = 4
+
+class UntexturedRepresentation(Enum):
+    InpaintingCompletion = 0
+    UniformColor = 1
+
+class TextureSource(Enum):
+    PhotosFirst = 0
+    PointCloudsFirst = 1
+    Smart = 2
+
+class Point3d:
+    def __init__(self, x = 0, y = 0, c = 0) -> None:
+        self.x = x
+        self.y = y
+        self.z = z
+
+class ReferenceModelOptions:
+    def __init__(self) -> None:
+        self.ref_model_type: ReferenceModelType = None
+        self.tiling_mode: TilingMode = None
+        self.tiling_value: float = None
+        self.tilingOrigin: Point3d = None
+        self.discard_empty_tiles: bool = None
+        self.srs: str = None
+        self.geometric_precision: GeometricPrecision = None
+        self.pair_selection: PairSelection = None
+        self.photo_used_for_geometry: PhotoUsedForGeometry = None
+        self.hole_filling: HoleFilling = None
+        self.simplification: Simplification = None
+        self.planar_simplification_tolerance: float = None
+        self.color_correction: ColorCorrection = None
+        self.untextured_representation: UntexturedRepresentation = None
+        self.untextured_color: str = None
+        self.texture_source = None #TextureSource
+        self.ortho_resolution: float = None
+        self.geometry_resolution_limit: float = None
+        self.texture_resolution_limit: float = None
+
+class ReferenceModel:
+    def __init__(self) -> None:
+        self.reference_model_path: str = None
+        self.options: ReferenceModelOptions = None
+
+
 class Options3MX:
     def __init__(self) -> None:
         self.texture_color_source: ColorSource = None
@@ -363,12 +449,13 @@ class ReconstructionSpecifications:
     class Inputs:
         def __init__(self) -> None:
             self.oriented_photos: str = None
+            self.reference_model: str = None
             #TODO : other inputs
 
     class Outputs:
         def __init__(self) -> None:
             self.exports: list[Export] = []
-            #TODO : do other outputs
+            self.reference_model: ReferenceModel = None
 
     class Options:
         def __init__(self) -> None:
@@ -387,54 +474,64 @@ class ReconstructionSpecifications:
 
         specifications = ReconstructionSpecifications()
         try:
-            specifications.inputs.oriented_photos = specifications_json["Inputs"]["OrientedPhotos"]
+            if "OrientedPhotos" in specifications_json["Inputs"]:
+                specifications.inputs.oriented_photos = specifications_json["Inputs"]["OrientedPhotos"]
+            if "ReferenceModel" in specifications_json["Inputs"]:
+                specifications.inputs.reference_model = specifications_json["Inputs"]["ReferenceModel"]
 
-            exports = []
-            for exports_json in specifications_json["Outputs"]["Exports"]:
-                export = Export()
-                export.export_path = exports_json["Path"]
-                if exports_json["Format"] == "3MX":
-                    export.format = Format.ThreeMX
-                if exports_json["Format"] == "3SM":
-                    export.format = Format.ThreeSM
-                if exports_json["Format"] == "Cesium3DTiles":
-                    export.format = Format.Cesium3DTiles
-                if exports_json["Format"] == "OSGB":
-                    export.format = Format.OSGB
-                if exports_json["Format"] == "SpacEyes":
-                    export.format = Format.SpacEyes
-                if exports_json["Format"] == "OBJ":
-                    export.format = Format.OBJ
-                if exports_json["Format"] == "S3C":
-                    export.format = Format.S3C
-                if exports_json["Format"] == "I3S":
-                    export.format = Format.I3S
-                if exports_json["Format"] == "LodTree":
-                    export.format = Format.LodTree
-                if exports_json["Format"] == "Collada":
-                    export.format = Format.Collada
-                if exports_json["Format"] == "OCP":
-                    export.format = Format.OCP
-                if exports_json["Format"] == "KML":
-                    export.format = Format.KML
-                if exports_json["Format"] == "DGN":
-                    export.format = Format.DGN
-                if exports_json["Format"] == "SuperMap":
-                    export.format = Format.SuperMap
-                if exports_json["Format"] == "Las":
-                    export.format = Format.Las
-                if exports_json["Format"] == "POD":
-                    export.format = Format.POD
-                if exports_json["Format"] == "Ply":
-                    export.format = Format.Ply
-                if exports_json["Format"] == "OPC":
-                    export.format = Format.OPC
-                if exports_json["Format"] == "OrthophotoDSM":
-                    export.format = Format.OrthophotoDSM
-                if exports_json["Format"] == "Touchup":
-                    export.format = Format.Touchup
-                exports.append(export)
-            specifications.outputs.exports = exports
+            if "Exports" in specifications_json["Outputs"]:
+                exports = []
+                for exports_json in specifications_json["Outputs"]["Exports"]:
+                    export = Export()
+                    export.export_path = exports_json["Path"]
+                    if exports_json["Format"] == "3MX":
+                        export.format = Format.ThreeMX
+                    if exports_json["Format"] == "3SM":
+                        export.format = Format.ThreeSM
+                    if exports_json["Format"] == "Cesium3DTiles":
+                        export.format = Format.Cesium3DTiles
+                    if exports_json["Format"] == "OSGB":
+                        export.format = Format.OSGB
+                    if exports_json["Format"] == "SpacEyes":
+                        export.format = Format.SpacEyes
+                    if exports_json["Format"] == "OBJ":
+                        export.format = Format.OBJ
+                    if exports_json["Format"] == "S3C":
+                        export.format = Format.S3C
+                    if exports_json["Format"] == "I3S":
+                        export.format = Format.I3S
+                    if exports_json["Format"] == "LodTree":
+                        export.format = Format.LodTree
+                    if exports_json["Format"] == "Collada":
+                        export.format = Format.Collada
+                    if exports_json["Format"] == "OCP":
+                        export.format = Format.OCP
+                    if exports_json["Format"] == "KML":
+                        export.format = Format.KML
+                    if exports_json["Format"] == "DGN":
+                        export.format = Format.DGN
+                    if exports_json["Format"] == "SuperMap":
+                        export.format = Format.SuperMap
+                    if exports_json["Format"] == "Las":
+                        export.format = Format.Las
+                    if exports_json["Format"] == "POD":
+                        export.format = Format.POD
+                    if exports_json["Format"] == "Ply":
+                        export.format = Format.Ply
+                    if exports_json["Format"] == "OPC":
+                        export.format = Format.OPC
+                    if exports_json["Format"] == "OrthophotoDSM":
+                        export.format = Format.OrthophotoDSM
+                    if exports_json["Format"] == "Touchup":
+                        export.format = Format.Touchup
+                    exports.append(export)
+                specifications.outputs.exports = exports
+
+            if "ReferenceModel" in specifications_json["Outputs"]:
+                reference_model = ReferenceModel()
+                reference_model.reference_model_path = specifications_json["Outputs"]["ReferenceModel"]["Path"]
+                specifications.outputs.reference_model = reference_model
+
         except Exception as e:
             return ReturnValue(value=specifications, error=str(e))
         return ReturnValue(value=specifications, error="")
