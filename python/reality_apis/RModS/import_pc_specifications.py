e0f679b342967ba9b08b23f34ff0ebb6c8a20689
dbiguenet <110406974+dbiguenet@users.noreply.github.com>
2024-10-30 16:40:11 +0100

    Add ImportPC specs.

---
+++ python/reality_apis/RModS/import_pc_specifications.py
@@ -0,0 +1,49 @@
+# Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+# See LICENSE.md in the project root for license terms and full copyright notice.
+
+from __future__ import annotations
+
+from reality_apis.utils import ReturnValue
+
+import json
+import os
+
+class ImportPCSpecifications:
+    def __init__(self) -> None:
+        self.inputs = self.Inputs()
+        self.outputs = self.Outputs()
+
+    def get_type(self) -> str:
+        return "ImportPC"
+
+    class Inputs:
+        def __init__(self) -> None:
+            self.scene: str = None
+
+    class Outputs:
+        def __init__(self) -> None:
+            self.scan_collection: str = None
+
+    @classmethod
+    def from_json_file(cls, json_file: str) -> ReturnValue[ImportPCSpecifications]:
+        if not os.path.isfile(json_file):
+            return ReturnValue(value=ImportPCSpecifications(), error="File not found: " + json_file)
+        try:
+            with open(json_file, encoding='utf-8') as f:
+                specifications_json = json.load(f)
+        except Exception as e:
+            return ReturnValue(value=ImportPCSpecifications(),
+                               error=f"Failed to load specifications {json_file}: {e}")
+
+        specifications = ImportPCSpecifications()
+
+        try:
+            if "scene" in specifications_json["inputs"]:
+                specifications.inputs.scene = specifications_json["inputs"]["scene"]
+
+            if "scanCollection" in specifications_json["outputs"]:
+                specifications.outputs.scan_collection = specifications_json["outputs"]["scanCollection"]
+
+        except Exception as e:
+            return ReturnValue(value=specifications, error=str(e))
+        return ReturnValue(value=specifications, error="")
--- python/reality_apis/__init__.py
+++ python/reality_apis/__init__.py
@@ -17,5 +17,6 @@ import reality_apis.RModS.calibration_specifications
 import reality_apis.RModS.tiling_specifications
 import reality_apis.RModS.production_specifications
 import reality_apis.RModS.fill_image_properties_specifications
+import reality_apis.RModS.import_pc_specifications
 import reality_apis.RModS.utils
 import reality_apis.utils
